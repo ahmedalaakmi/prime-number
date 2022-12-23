eb5c9caea3c4e51afd6d6aa5f0030f77cb2f495c
ahmedalaakmi <121300886+ahmedalaakmi@users.noreply.github.com>
2022-12-23 16:10:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter n1: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter n2: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            for (int i = n1; i <= n2; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static bool IsPrime(int number)
+        {
+            if (number < 2) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            int boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
